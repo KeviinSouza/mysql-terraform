57c0a12eabdda79b1379b8425f953002d77e679c
Kevin Souza <kevinsou113@gmail.com>
2021-10-26 20:15:20 -0300

    Arquivos de config provider/network

---
+++ network.tf
@@ -0,0 +1,38 @@
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+    name                 = "subnet"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes       = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+    name                         = "publicip"
+    location                     = azurerm_resource_group.rg.location
+    resource_group_name          = azurerm_resource_group.rg.name
+    allocation_method            = "Static"
+}
+
+resource "azurerm_network_security_group" "sg" {
+    name                = "sg"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
